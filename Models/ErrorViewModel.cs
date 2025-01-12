e9dcb40c84af69b9faac573293c89c0e74541ee4
YSFather <basketlinux@nate.com>
2025-01-13 01:39:29 +0900

    초기 커밋.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WetCoreTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
